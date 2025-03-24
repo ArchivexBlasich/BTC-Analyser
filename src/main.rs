d0b19fe6dc39951d7bb43a33aa8e3c95cf36a807
ArchivexBlasich <fblasich0@gmail.com>
2025-03-24 13:44:48 -0300

    inspect address option finished

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use ansi_term::Color::{self, Red};
 use chrono::NaiveTime;
 use clap::Parser;
-use cli_table::{print_stdout, Cell, Style, Table, WithTitle};
+use cli_table::{print_stdout, Cell, Style, Table};
+use num_format::{Locale, ToFormattedString};
 use serde::Deserialize;
 use serde_json::Value;
 
@@ -22,21 +23,17 @@ struct Cli {
 
     #[arg(short = 'i', long)]
     inspect_transcation: Option<String>,
+
+    #[arg(short = 'a', long)]
+    inspect_address: Option<String>,
 }
 
 
-#[derive(Debug, Table)]
+#[derive(Debug)]
 struct UndefinedTransaction {
-    #[table(title = "Hash", color = "cli_table::Color::Yellow")]
     hash: String,
-
-    #[table(title = "Bitcoin", color = "cli_table::Color::Yellow")]
     amount_bitcoin: f64,
-
-    #[table(title = "Amount(USD)", color = "cli_table::Color::Yellow")]
     amount_usd: f64,
-
-    #[table(title = "Time", color = "cli_table::Color::Yellow")]
     time: NaiveTime,
 }
 
@@ -64,12 +61,9 @@ struct Input {
     prev_out: PrevOut,
 }
 
-#[derive(Debug, Deserialize, Table)]
+#[derive(Debug, Deserialize)]
 struct PrevOut {
-    #[table(title = "Address (input)", color = "cli_table::Color::Blue")]
     addr: String,
-
-    #[table(title = "Value", color = "cli_table::Color::Blue")]
     value: u64,
 }
 
@@ -77,6 +71,14 @@ struct PrevOut {
 struct Output {
     value: u64,
     addr: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct BitcoinAddress {
+    n_tx: u64, // Number of transaction that this address made
+    total_received: u64,
+    total_sent: u64,
+    final_balance: u64,
 }
 
 const SATOSHIS_PER_BTC: u32 = 100_000_000;
@@ -98,8 +100,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let unconfirmed_transactions_url =
         "https://blockchain.info/unconfirmed-transactions?format=json";
     let inspect_transaction_url = "https://blockchain.info/rawtx/";
-    let _inspect_address_url = "https://www.blockchain.com/explorer/addresses/btc/";
-    let bitcoin_price_url = "https://blockchain.info/rawaddr/";
+    let inspect_address_url = "https://blockchain.info/rawaddr/";
+    let bitcoin_price_url = "https://api.blockchain.info/stats" ;
+    
 
     match cli.exploration_mode.as_deref() {
             // We check if the user specified a number of outputs, by default is 100
@@ -114,12 +117,34 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 unconfirmed_transactions(unconfirmed_transactions_url, bitcoin_price_url, number_outputs).await?;
 
             // Print the table
-            print_stdout(undefined_transaction_vec.with_title().foreground_color(Some(cli_table::Color::Yellow)))?;
+            let table: Vec<_> = undefined_transaction_vec.iter().map(|undefined_transaction| {
+                vec![
+                    Color::Yellow.paint(undefined_transaction.hash.clone()).cell(), 
+                    Color::Yellow.paint(format!("{} BTC", undefined_transaction.amount_bitcoin)).cell(),
+                    Color::Yellow.paint(format!("${}{:.5}", (undefined_transaction.amount_usd.trunc() as u64).to_formatted_string(&Locale::en), {undefined_transaction.amount_usd.fract()})).cell(),
+                    Color::Yellow.paint(format!("{}", undefined_transaction.time.format("%H:%M"))).cell(),
+                ]
+            }).collect();
+
+            let undefined_transaction_table = cli_table::Table::table(table)
+                .title(vec![
+                    "Hash".cell().bold(true),
+                    "Bitcoin".cell().bold(true),
+                    "Amount(USD)".cell().bold(true),
+                    "Time".cell().bold(true),
+                ])
+                .foreground_color(Some(cli_table::Color::Yellow));
+
+            print_stdout(undefined_transaction_table)?;
+            println!();
 
             // Show the total amount of money that was transfer
             let total:f64 = undefined_transaction_vec.iter().map(|ut| ut.amount_usd).sum();
             let table = vec![
-                vec!["Total Amount".cell(), format!("${}", total.to_string()).cell()]
+                vec![
+                    Color::Purple.paint("Total Amount").cell(), 
+                    Color::Purple.paint(format!("${}{:.5}", (total.trunc() as u64).to_formatted_string(&Locale::en), {total.fract()})).cell(),
+                ]
             ].table();
 
             print_stdout(table.foreground_color(Some(cli_table::Color::Magenta)))?;
@@ -148,14 +173,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
                     // Show Total inputs table
                     let totals_table = vec![
-                        vec![format!("{} BTC", total_input.to_string()).cell(), format!("{} BTC", total_output.to_string()).cell()]
+                        vec![
+                            Color::Yellow.paint(format!("{} BTC",total_input.to_string())).cell(), 
+                            Color::Yellow.paint(format!("{} BTC", total_output.to_string())).cell()
+                        ]
                     ]
                     .table()
                     .title(vec![
-                        "Total Input".cell(),
-                        "Total Output".cell(),
-                    ])
-                    .bold(true);
+                        "Total Input".cell().bold(true),
+                        "Total Output".cell().bold(true),
+                    ]);
 
                     print_stdout(totals_table.foreground_color(Some(cli_table::Color::Yellow)))?;
 
@@ -163,13 +190,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     // Show Address inputs and its Value table
                     let table: Vec<_> = total_inputs.iter().map(|prev_out| {
                         let btc = prev_out.value as f64 / SATOSHIS_PER_BTC as f64;
-                        vec![prev_out.addr.clone().cell(), format!("{} BTC", btc.to_string()).cell()]
+                        vec![
+                            Color::Green.paint(prev_out.addr.clone()).cell(), 
+                            Color::Green.paint(format!("{} BTC", btc.to_string())).cell()
+                        ]
                     }).collect();
 
                     let table_inputs = cli_table::Table::table(table)
                         .title(vec![
-                            "Address (input)".cell(),
-                            "Value".cell()
+                            "Address (input)".cell().bold(true),
+                            "Value".cell().bold(true),
                         ])
                         .foreground_color(Some(cli_table::Color::Green));
 
@@ -179,13 +209,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     // Show Address Outputs and its Value table
                     let table: Vec<_> = total_outputs.iter().map(|output| {
                         let btc = output.value as f64 / SATOSHIS_PER_BTC as f64;
-                        vec![output.addr.clone().cell(), format!("{} BTC", btc.to_string()).cell()]
+                        vec![
+                            Color::Green.paint(output.addr.clone()).cell(), 
+                            Color::Green.paint(format!("{} BTC", btc.to_string())).cell()
+                        ]
                     }).collect();
 
                     let table_outputs = cli_table::Table::table(table)
                         .title(vec![
-                            "Address (output)".cell(),
-                            "Value".cell()
+                            "Address (output)".cell().bold(true),
+                            "Value".cell().bold(true),
                         ])
                         .foreground_color(Some(cli_table::Color::Green));
 
@@ -195,7 +228,60 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             };
         }
         Some("address") => {
-            println!("Inspecting a transaction address...");
+            if let None = cli.inspect_address {
+                println!("{}", Color::Cyan.paint("Provide a Bitcoin Addres (i.e -a bc1q9jk7j04lcjzdns6wjegeju78mxq27reg5e4wkycrs407yd0r07psvs8x7u)\n"));
+                help_panel();
+                std::process::exit(1);
+            }
+
+            match inspect_address(&cli.inspect_address.unwrap(), inspect_address_url).await {
+                Ok(bitcoin_address) => {
+                    let bitcoin_price_query =  reqwest::get(bitcoin_price_url)
+                            .await
+                            .unwrap()
+                            .text()
+                            .await
+                            .unwrap();
+
+                    let parsed: Value = serde_json::from_str(&bitcoin_price_query)?;
+                    let bitcoin_price = parsed["market_price_usd"].as_f64().unwrap();
+
+                    let bitcoin_address_table =  {
+                        let total_received_btc = bitcoin_address.total_received as f64 / SATOSHIS_PER_BTC as f64;
+                        let total_received_usd = total_received_btc * bitcoin_price;
+                        let total_sent_btc = bitcoin_address.total_sent as f64 / SATOSHIS_PER_BTC as f64;
+                        let total_sent_usd = total_sent_btc * bitcoin_price;
+                        let final_balance_btc = bitcoin_address.final_balance as f64 / SATOSHIS_PER_BTC as f64;
+                        let final_balance_usd = final_balance_btc * bitcoin_price;
+                        vec![
+                            vec![
+                                Color::Cyan.paint(bitcoin_address.n_tx.to_string()).cell(),
+                                Color::Cyan.paint(format!("{} BTC", total_received_btc)).cell(),
+                                Color::Cyan.paint(format!("{} BTC", total_sent_btc)).cell(),
+                                Color::Cyan.paint(format!("{} BTC", final_balance_btc)).cell(),
+                            ],
+                            vec![
+                                " ".cell(),
+                                Color::Cyan.paint(format!("${}{:.5}", (total_received_usd.trunc() as u64).to_formatted_string(&Locale::en), {total_received_usd.fract()})).cell(),
+                                Color::Cyan.paint(format!("${}{:.5}", (total_sent_usd.trunc() as u64).to_formatted_string(&Locale::en), {total_sent_usd.fract()})).cell(),
+                                Color::Cyan.paint(format!("${}{:.3}", (final_balance_usd.trunc() as u64).to_formatted_string(&Locale::en), {final_balance_usd.fract()})).cell(),
+                            ],
+                        ]
+                        .table()
+                        .title(vec![
+                            "Transactions Made".cell().bold(true),
+                            "Total Amount Received".cell().bold(true),
+                            "Total Amount Sent".cell().bold(true),
+                            "Total Balance in the Account".cell().bold(true),
+                        ])
+                        .foreground_color(Some(cli_table::Color::Cyan))
+                    };
+
+                    print_stdout(bitcoin_address_table)?;
+                    println!();
+                },
+                Err(_) => println!("{}",Color::Red.paint("[!] There is not transaction with the hash received")),
+            }
         }
         _ => {
             help_panel();
@@ -207,7 +293,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 fn help_panel() {
-    println!("{}", Color::Red.paint("[!] Usage:  ./btcAnalyser -e [-n] [-i]"));
+    println!("{}", Color::Red.paint("[!] Usage:  ./btcAnalyser -e [-n] [-i] [-a]"));
     println!("{}", Color::Red.paint("---------------------------------------------------------------------------------------------------"));
     println!("\n\t{}", Color::Yellow.paint("[-e] Exploration Mode"));
     println!(
@@ -236,6 +322,12 @@ fn help_panel() {
         "\t\t{}\t{}",
         Color::Purple.paint("Example:"),
         Color::Yellow.paint("./btcAnalyser -e inspect -i 136937e5a742645ce873f079f8668aefdc2d06b8172e903d031a8bfb48969450")
+    );
+    println!("\n\t{}", Color::Yellow.paint("[-a] Provide the Bitcoin Address"));
+    println!(
+        "\t\t{}\t{}",
+        Color::Purple.paint("Example:"),
+        Color::Yellow.paint("./btcAnalyser -e address -a bc1q9jk7j04lcjzdns6wjegeju78mxq27reg5e4wkycrs407yd0r07psvs8x7u")
     );
     println!();
 }
@@ -338,5 +430,29 @@ async fn inspect_transcation(
     Ok(transaction)
 }
 
+async fn inspect_address(
+    bitcoin_address: &str,
+    inspect_address_url: &str,
+) -> Result<BitcoinAddress, Box<dyn std::error::Error>> {
+    let inspect_address_url = format!("{inspect_address_url}{bitcoin_address}");
+
+    let inspect_address_query = async {
+        let response = reqwest::get(&inspect_address_url).await?;
+        
+        if !response.status().is_success() {
+            return Err(Box::<dyn std::error::Error>::from("There is no transaction with the provided hash"));
+        }
+
+        response.text().await.map_err(|e| e.into())
+    };
+
+    let inspect_addres_json = tokio::join!(inspect_address_query);
+
+    // Serialize the JSON
+    let bitcoin_address: BitcoinAddress = serde_json::from_str(&inspect_addres_json.0?)?;
+
+
+    Ok(bitcoin_address)
+}
 
 
